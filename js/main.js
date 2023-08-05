cd1cf04108304801d9233a0e4999f631716f69e1
PhynxMorales <80421800+PhynxMorales@users.noreply.github.com>
2023-08-05 07:29:03 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,15 +4,35 @@ function updateHeaderClass() {
   const header = document.getElementById("mainHeader");
   const scrollY = window.scrollY;
   const threshold = 100; // Ajusta este valor según desees la distancia para aplicar la clase
+  const whatsapp = document.querySelector(".whatsapp-button");
 
   if (scrollY > threshold) {
     header.classList.add("nav-scroll");
     logo.src = "./assets/logo-color.png";
+    whatsapp.classList.add("show-top");
   } else {
     header.classList.remove("nav-scroll");
     logo.src = "./assets/logo-blanco.png";
+    whatsapp.classList.remove("show-top");
   }
 }
 
 // Agregar un evento de scroll para llamar a la función
 window.addEventListener("scroll", updateHeaderClass);
+
+let videoTour = document.getElementById("videoTour");
+
+// Obtener el modal por su ID
+
+const videoBusiness = document.getElementById("videoTour"); //Modal Video Business
+$(".contact__play").click(function () {
+  videoBusiness.play();
+});
+$(".btn-close").click(function () {
+  videoBusiness.pause();
+});
+$("#videoFuerteAventura").click(function () {
+  if ($(this).hasClass("show")) {
+    videoBusiness.pause();
+  }
+});
